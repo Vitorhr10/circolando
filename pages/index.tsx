1fce7078b4b2a3eb75cd28c98e7094e9533db667
vitorhr10 <vitorrodrigueshr10@mail.com>
2020-09-24 13:55:18 -0300

    info project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useState } from 'react';
-import {Flex, Image, Button, Text } from '@chakra-ui/core'
+import {Flex, Image, Button, Text, Grid } from '@chakra-ui/core'
 import Input from '../components/Input'
 import axios from 'axios';
 
@@ -22,45 +22,85 @@ export default function Home() {
       size="100%"
     >
 
-      <Flex gridArea="logo" flexDir="row" alignItems="flex-start">
+      <Flex 
+        gridArea="header" 
+        flexDir="row" 
+        alignItems="flex-start"
+      >
         <Image src="/circolando-header.jpg" alt="Circolando" />
       </Flex>
- 
+
       <Flex 
-        as="form"
-        onSubmit={handleSignUpToNewsletter}
-        backgroundColor="gray.700"
-        borderRadius="md"
-        flexDir="column"
-        alignItems="stretch"
-        padding={8}
-        marginTop={4}
-        width="100%" 
-        maxW="400px"
+        gridArea="text" 
+        flexDir="row" 
+        align="top"
       >
-        <Image marginBottom={2} src="/institutoagronelli.png" alt="Instituto Agronelli"/>
-  
-        <Text textAlign="center" fontSize="lg" color="#FFFFFF" marginBottom={2}>
-          Insira seu e-mail e receba o Livreto do Circolando para colorir!
-        </Text>
-  
-        <Input
-          placeholder="Seu e-mail"
-          marginTop={2}
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-  
-        <Button
-          type="submit"
-          backgroundColor="#194E25"
-          height="50px"
-          borderRadius="sm"
-          marginTop={6}
-          _hover={{ backgroundColor: '#288B45' }}
+
+        <Flex
+          gridArea="text" 
+          flexDir="column" 
+          align="top"
+          maxW="600px"
+          margin="10"
+
         >
-          QUERO RECEBER
-        </Button>
+          <Text textAlign="center" textTransform="uppercase" fontSize="xl" color="#FFFFFF" marginBottom={10}>
+            Programação dia das crianças
+          </Text>
+
+          <Text fontSize="md" color="#FFFFFF">
+            A programação da "Semana da Criança" em Uberaba conta com diversas atividades para toda a população. Os eventos da Prefeitura de Uberaba, que acontecem até o próximo domingo (13), contam com apresentações musicais, brincadeiras, palestras, gincanas educativas, entre outros. Todas as atividades são gratuitas e os menores de idade devem estar acompanhados de um adulto responsável.
+          </Text>   
+          
+          <br></br>
+
+          <Text fontSize="md" color="#FFFFFF">
+            As atividades programadas para a semana comemorativa começaram nesta segunda-feira (7), com o projeto de Conscientização Ambiental, na reabertura do Zoológico. O Parque do Jacarandá estava fechado para reformas de adequação. Além disso, desde o dia 2 até o dia 18 de outubro, palestras sobre educação infantil são apresentadas para a comunidade, voltada para pais e educadores.
+          </Text>  
+
+          <br></br>
+
+          <Text fontSize="md" color="#FFFFFF">
+            As atividades programadas para a semana comemorativa começaram nesta segunda-feira (7), com o projeto de Conscientização Ambiental, na reabertura do Zoológico. O Parque do Jacarandá estava fechado para reformas de adequação. Além disso, desde o dia 2 até o dia 18 de outubro, palestras sobre educação infantil são apresentadas para a comunidade, voltada para pais e educadores.
+          </Text> 
+        </Flex>
+        
+        <Flex 
+          as="form"
+          onSubmit={handleSignUpToNewsletter}
+          backgroundColor="gray.700"
+          borderRadius="md"
+          flexDir="column"
+          alignItems="stretch"
+          padding={8}
+          marginTop={4}
+          width="100%" 
+          maxW="400px"
+        >
+          <Image marginBottom={2} src="/institutoagronelli.png" alt="Instituto Agronelli"/>
+    
+          <Text textAlign="center" fontSize="lg" color="#FFFFFF" marginBottom={2}>
+            Insira seu e-mail e receba o Livreto do Circolando para colorir!
+          </Text>
+    
+          <Input
+            placeholder="Seu e-mail"
+            marginTop={2}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            />
+    
+          <Button
+            type="submit"
+            backgroundColor="#194E25"
+            height="50px"
+            borderRadius="sm"
+            marginTop={6}
+            _hover={{ backgroundColor: '#288B45' }}
+            >
+            QUERO RECEBER
+          </Button>
+        </Flex>
       </Flex>
 
       <Flex 
@@ -70,7 +110,7 @@ export default function Home() {
         alignItems="stretch"
         padding={4}
         marginTop={4}
-        width="100%" 
+        width="100%"
       >
 
         <Text textAlign="center" marginTop={2} fontSize="sm" color="#FFFFFF" marginBottom={2}>
